1f0672410b297be8b08c88728173335b69b0f0bc
overseer <108105465+OverseerUniverse@users.noreply.github.com>
2026-05-18 13:38:10 -0700

    Refactor the logout button to be reuseable

--- src/components/logout/LogoutButton.js
+++ src/components/logout/LogoutButton.js
@@ -1,9 +1,24 @@
 "use client"
 
 import { useRouter } from "next/router"
-import { logout } from "@/utils/logout"
 
-export default function LogoutButton() {
+// make sure to update the state of the component with the logout button
+// based on whether the user is loggedin or not.
+// that way the ui properly updates, simply useState()
+// example: <LogoutButton onLogout={() => setLoggedIn(false)} />
+
+export default function LogoutButton({ onLogout }) {
   const router = useRouter
-  return <button onClick={() => logout(router)}>Logout</button>
+
+  function handleClick() {
+    localStorage.removeItem("token")
+
+    if (onLogout) {
+      // executes the function passed from the parent
+      onLogout()
+    }
+
+    router.push("/login")
+  }
+  return <button onClick={handleClick}>Logout</button>
 }
--- src/utils/logout.js
+++
@@ -1,4 +0,0 @@
-export default function logout(router) {
-  localStorage.removeItem("token")
-  router.push("/login")
-}

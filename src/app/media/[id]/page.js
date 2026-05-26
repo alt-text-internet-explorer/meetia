241b900c8f6c27c485817f1ea2ec5ce148137792
bvarni-calpoly <bvarni@calpoly.edu>
2026-05-26 21:05:51 +0000

    changed to use bootstrap

--- src/app/media/[id]/page.js
+++ src/app/media/[id]/page.js
@@ -2,10 +2,13 @@ import React from "react"
 import Link from "next/link"
 import MediaGallery from "@/components/media/Media"
 
-const Profile = () => {
+const Profile = async ({ params }) => {
+  
+  const { id } = await params;
+
   return (
     <div>
-      <MediaGallery />
+      <MediaGallery id={id} />
     </div>
   )
 }
